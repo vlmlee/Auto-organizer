350365eff4844e1f06a0019178a1d9be25d55047
vlmlee <mlee@vellichorlabs.com>
2016-07-18 20:31:13 -0800

    Removed contents

--- test/test.js
+++ test/test.js
@@ -1,15 +1,1 @@
-var file = './test.txt';
-var fs = require('fs');
 
-var contents = fs.readFile(file, 'utf8', function(err, content) {
-    var postTitle = new RegExp('\<.*?\>', 'g').exec(content)[0].removeCharContainers();
-    var postBlob = postTitle.split(' ').join('-');
-    var postBody = new RegExp('\{.*?\}', 'g').exec(content)[0].removeCharContainers();
-    var postDate = Date.now();
-
-    console.log(postDate);
-});
-
-String.prototype.removeCharContainers = function() {
-    return this.slice(1, -1).trim();
-}
